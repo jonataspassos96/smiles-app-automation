f58df10a99248d5503e3fb17f72373223cd072b6
Jonatas Passos <jpc080496@gmail.com>
2022-08-20 17:18:22 -0300

    test: create case 'Usuário esqueceu sua senha'

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -56,4 +56,32 @@ describe('Login', () => {
         const errorMessage = await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/dialog_smiles_text"]')
         expect(errorMessage).toHaveText('Usuário não encontrado ou Senha está inválida.')
     })
+
+    it('Usuário esqueceu sua senha', async () => {
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]').click()
+
+        await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/btn_intro_jump"]').click()
+
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_and_dont_ask_again_button"]').click()
+
+        await $('//android.widget.Button[@resource-id="com.pontomobi.smileshmg:id/lgpd_accept_agreed"]').click()
+
+        const title = await $('//android.widget.TextView[@text="Seja bem-vindo."]')
+
+        await expect(title).toHaveText('Seja bem-vindo.')
+
+        await $('~btn_login_btn_signIn').click()
+
+        await $('~btn_login_forgot_password').click()
+
+        const loginInput = await $('//android.widget.EditText[@resource-id="com.pontomobi.smileshmg:id/editText"]')
+        await loginInput.setValue('123 479 425')
+
+        await $('//android.widget.Button[@resource-id="com.pontomobi.smileshmg:id/positive_confirmation_submit_btn"]').click()
+
+        const email = await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/fragment_forgot_password_registered_email"]')
+        expect(email).toHaveText('****mat@mat.com.br')
+
+        // await $('//android.widget.Button[@resource-id="com.pontomobi.smileshmg:id/reset_password_submit_btn"]').click()
+    })
 })
