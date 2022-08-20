590f118958f3a360d5f0d1f2a2340591f5e02229
Jonatas Passos <jpc080496@gmail.com>
2022-08-20 17:17:47 -0300

    test: create case 'Deve exibir mensagem de erro ao digitar dados inválidos'

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -31,4 +31,29 @@ describe('Login', () => {
         const userName = await $('//android.widget.TextView[@text="Olá Matias"]')
         expect(userName).toHaveText('Olá Matias')
     })
+
+    it('Deve exibir mensagem de erro ao digitar dados inválidos', async () => {
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]').click()
+
+        await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/btn_intro_jump"]').click()
+
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_and_dont_ask_again_button"]').click()
+
+        await $('//android.widget.Button[@resource-id="com.pontomobi.smileshmg:id/lgpd_accept_agreed"]').click()
+
+        const title = await $('//android.widget.TextView[@text="Seja bem-vindo."]')
+
+        await expect(title).toHaveText('Seja bem-vindo.')
+
+        await $('~btn_login_btn_signIn').click()
+
+        const loginInput = await $('~txt_login_input')
+        await loginInput.setValue('123 479 425')
+
+        const passwordInput = await $('~txt_login_password_input')
+        await passwordInput.setValue('1234')
+
+        const errorMessage = await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/dialog_smiles_text"]')
+        expect(errorMessage).toHaveText('Usuário não encontrado ou Senha está inválida.')
+    })
 })
