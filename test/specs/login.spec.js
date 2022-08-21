8c6659d073f3667c950f8f3e402a855188e97f0c
Jonatas Passos <jpc080496@gmail.com>
2022-08-21 19:47:39 -0300

    test: create scenery 'Esqueceu seu número Smiles'

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -120,3 +120,37 @@ describe('Esqueceu sua senha', () => {
         expect(textCpf).toHaveText('CPF inválido')
     })
 })
+
+describe.only('Esqueceu seu número Smiles', async () => {
+    beforeEach(async () => {
+        await driver.launchApp();
+
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]').click()
+
+        await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/btn_intro_jump"]').click()
+
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_and_dont_ask_again_button"]').click()
+
+        await $('//android.widget.Button[@resource-id="com.pontomobi.smileshmg:id/lgpd_accept_agreed"]').click()
+
+        const title = await $('//android.widget.TextView[@text="Seja bem-vindo."]')
+
+        await expect(title).toHaveText('Seja bem-vindo.')
+
+        await $('~btn_login_btn_signIn').click()
+
+        await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/login_forgot_smiles_number"]').click()
+    })
+
+    it('Deve inserir dados inválidos', async () => {
+        const inputCpf = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.EditText')
+        inputCpf.setValue('431 549 334 17')
+
+        const inputBirthDate = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.EditText')
+        inputBirthDate.setValue('08/04/1996')
+
+        // ERROR
+        const text = await $('//android.widget.TextView[@resource-id="com.pontomobi.smileshmg:id/md_content"]')
+        expect(text).toHaveText('xablau')
+    })
+})
