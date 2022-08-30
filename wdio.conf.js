aecbea17dc7b65968be82bd81248e0598f349a43
Jonatas Passos <jpc080496@gmail.com>
2022-08-30 11:52:28 -0300

    refactor: change reporters

--- wdio.conf.js
+++ wdio.conf.js
@@ -127,9 +127,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: [['allure', {
-        outputDir: 'allure-results',
-    }]],
+    reporters: ['spec'],
 
 
 
